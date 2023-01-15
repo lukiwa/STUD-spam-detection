9ebad821658484d1c44b9dadf4efe0ed29eae5d8
lukiwa <lukasz.iwan.99@gmail.com>
2023-01-16 00:36:48 +0100

    Add multithreaded kfold

--- main.py
+++ main.py
@@ -21,10 +21,11 @@ from imblearn.under_sampling import RandomUnderSampler
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.model_selection import StratifiedKFold
 
-
 # misc
 from tqdm import tqdm
 from time import time
+from copy import copy
+from threading import Thread
 
 
 def timer_func(func):
@@ -78,40 +79,8 @@ class MockPreprocessor:
         return a, b
 
 
-def perform_classification():
-    pass
-
-def main():
-    # prepare dataset from csv
-    dataset = get_dataset("spam_ham_dataset.csv")
-
-    # print (im)balance
-    dataset_info(dataset)
-
-    # remove stopwords - words that bring no meaning to the text (etc. I, is, are, the)
-    remove_stopwords(dataset)
-
-    X = dataset['text']
-    y = dataset['class']
-    clf = MultinomialNB()
-    vector = CountVectorizer()
-    cv = StratifiedKFold(n_splits=2)
-
-    preprocessors = {
-        'Without': MockPreprocessor(1337),
-        # 'SMOTE': SMOTE(random_state=1337),
-        'Undersampling': RandomUnderSampler(random_state=1337),
-        # 'Oversampling': RandomOverSampler(random_state=1337)
-    }
-    scores = {
-        'Without': {'f1_score': [], 'accuracy': [], 'precision': [], 'recall': []},
-        'SMOTE': {'f1_score': [], 'accuracy': [], 'precision': [], 'recall': []},
-        'Undersampling': {'f1_score': [], 'accuracy': [], 'precision': [], 'recall': []},
-        'Oversampling': {'f1_score': [], 'accuracy': [], 'precision': [], 'recall': []}
-    }
-
-    for name, preprocessor in preprocessors.items():
-        for train_idx, test_idx, in tqdm(cv.split(X, y), total=cv.get_n_splits(), desc=name):
+def perform_classification(X, y, scores, clf, name, preprocessor, cv, vector, position):
+    for train_idx, test_idx, in tqdm(cv.split(X, y), total=cv.get_n_splits(), desc=name, position=position):
             X_train, y_train = X[train_idx], y[train_idx]
             X_test, y_test = X[test_idx], y[test_idx]
 
@@ -134,6 +103,46 @@ def main():
             scores[name]['precision'].append(round(precision_score(y_test, pred) * 100, 2))
             scores[name]['recall'].append(round(recall_score(y_test, pred) * 100, 2))
 
+
+def main():
+    # prepare dataset from csv
+    dataset = get_dataset("spam_ham_dataset.csv")
+
+    # print (im)balance
+    dataset_info(dataset)
+
+    # remove stopwords - words that bring no meaning to the text (etc. I, is, are, the)
+    remove_stopwords(dataset)
+
+    X = dataset['text']
+    y = dataset['class']
+    clf = MultinomialNB()
+    vector = CountVectorizer()
+    cv = StratifiedKFold(n_splits=5)
+
+    preprocessors = {
+        'Without': MockPreprocessor(1337),
+        'SMOTE': SMOTE(random_state=1337),
+        'Undersampling': RandomUnderSampler(random_state=1337),
+        'Oversampling': RandomOverSampler(random_state=1337)
+    }
+    scores = {
+        'Without': {'f1_score': [], 'accuracy': [], 'precision': [], 'recall': []},
+        'SMOTE': {'f1_score': [], 'accuracy': [], 'precision': [], 'recall': []},
+        'Undersampling': {'f1_score': [], 'accuracy': [], 'precision': [], 'recall': []},
+        'Oversampling': {'f1_score': [], 'accuracy': [], 'precision': [], 'recall': []}
+    }
+
+    threads = []
+    for name, preprocessor in preprocessors.items():
+        thread = Thread(target=perform_classification, args=(X, y, scores, copy(clf), name, preprocessor, cv, copy(vector), len(threads)))
+        threads.append(thread)
+        thread.start()    
+        #thread.join() # uncomment if having memory issues
+    
+    for thread in threads:
+        thread.join()
+
     # calculate meanscores
     mean_scores = {
         'Without': {'f1_score': 0, 'accuracy': 0, 'precision': 0, 'recall': 0},
@@ -146,7 +155,8 @@ def main():
             mean_scores[key][measurements_key] = np.round(
                 np.mean(measurements_values), 2)
 
-    print("Mean scores:")
+    # print results
+    print("Mean scores:", flush=True)
     for key, value in mean_scores.items():
         print(key, ": ")
         for measurements_key, measurements_value in value.items():
